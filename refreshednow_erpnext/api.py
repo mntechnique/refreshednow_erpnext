31f0d76e2157f78bc17cb1fa651a2a632a6a28d8
Vishal <vishaldhayagude07@gmail.com>
2016-12-07 16:53:42 +0530

    [new]Customer Lookup page added

--- refreshednow_erpnext/api.py
+++ refreshednow_erpnext/api.py
@@ -8,3 +8,52 @@ def get_available_cleaners():
 @frappe.whitelist()
 def get_daily_allocation():
 	return "Yeshwant 'Nana' Shinde"
+
+@frappe.whitelist()
+def get_caller_number(caller_number):
+	msg = []
+	cname = frappe.db.get_value("Contact",{"mobile_no":caller_number},"customer")
+	if cname:
+		#Create stub lead if lead is not found.
+		c = frappe.get_doc("Customer",cname)
+		out = frappe._dict({"name":c.customer_name,
+								"mobile_number":caller_number,
+								"phone":frappe.db.get_value("Contact",{"mobile_no":caller_number},"phone"),
+								"caller_type":"Customer",
+								"email_id":frappe.db.get_value("Contact",{"mobile_no":caller_number},"email_id")})
+		return out
+	lname = frappe.db.get_value("Lead",{"mobile_no":caller_number},"name")
+	if lname:
+		l = frappe.get_doc("Lead",lname)
+		out = frappe._dict({"name":l.lead_name,
+							"mobile_number":caller_number,
+							"phone":l.phone,
+							"email_id":l.email_id,
+							"caller_type":"Lead"
+			})
+		return out
+	return frappe._dict({"name":"New Lead","caller_type":"New Lead"})
+
+
+# @frappe.whitelist()
+# def get_email_list():
+# 	import email
+# 	email_queue_list = frappe.get_list("Email Queue", fields=["name", "recipient", "modified", "message", "acumen_email_subject"], filters={"status":"Sent"})
+
+# 	rmsg = []
+
+# 	for email_queue in email_queue_list:
+# 		b = email.message_from_string(email_queue.message)
+
+# 		if b['Subject'].startswith("Weekly status"):
+# 			rmsg.append(
+# 				frappe._dict({
+# 					"name": email_queue.name,
+# 					"recipient":email_queue.recipient,
+# 					"content":render_html_email_msg(email_queue.name),
+# 					"modified": frappe.utils.data.getdate(email_queue.modified)
+# 				}),
+# 			)
+
+# 	return rmsg
+
---
+++ refreshednow_erpnext/refreshednow_erpnext/page/customer_lookup/customer_lookup.js
@@ -0,0 +1,38 @@
+frappe.pages['customer-lookup'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Customer Lookup',
+		single_column: true
+	});
+
+	var content = null;
+	/*content = page.wrapper.find(".panel");
+	console.log(content.find("#"))*/
+
+	content = page.wrapper.find(".page-content");
+	var input_html= '<div class="input-group"> <input id="txt-lookup" type="text" class="form-control" placeholder="Search for caller number..."> <span class="input-group-btn"> <button id="btn-lookup" class="btn btn-secondary" type="button">Search!</button> </span> </div>'
+	content.append(input_html);
+	var btn = content.find('#btn-lookup');
+	var txt = content.find('#txt-lookup');
+	console.log(btn);
+	btn.click(function() {
+		frappe.call({
+			method: "refreshednow_erpnext.api.get_caller_number",
+			args: {
+				caller_number: $(txt).val()
+			},
+			freeze: true,
+			freeze_message: __("Retrieving Caller Details"),
+			callback: function(r) {
+				console.log(r.message);
+
+				content.append(frappe.render_template("caller_information", {"caller_info": r.message}));
+
+			}
+		});
+	});
+
+
+
+
+}
