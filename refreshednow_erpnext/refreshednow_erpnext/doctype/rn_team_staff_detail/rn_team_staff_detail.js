69425a542fef4ec4241c0da3eafff564b79c7c58
Vishal <vishaldhayagude07@gmail.com>
2016-12-16 23:15:06 +0530

    [new] Team Doctype Added

---
+++ refreshednow_erpnext/refreshednow_erpnext/doctype/rn_team_staff_detail/rn_team_staff_detail.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, MN Technique and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('RN Team Staff Detail', {
+	refresh: function(frm) {
+
+	}
+});
