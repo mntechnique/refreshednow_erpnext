edc3dd5a38004479012b804cf5e1b86e9e575042
Gaurav Naik <gvrvnk@gmail.com>
2016-12-22 17:29:15 +0530

    Fixed append issue in customer lookup page

--- refreshednow_erpnext/refreshednow_erpnext/page/customer_lookup/customer_lookup.js
+++ refreshednow_erpnext/refreshednow_erpnext/page/customer_lookup/customer_lookup.js
@@ -5,17 +5,16 @@ frappe.pages['customer-lookup'].on_page_load = function(wrapper) {
 		single_column: true
 	});
 
+	//Append search textbox and button
 	var content = null;
-	/*content = page.wrapper.find(".panel");
-	console.log(content.find("#"))*/
-
 	content = page.wrapper.find(".page-content");
 	console.log(content);
 	var input_html= '<div class="input-group"> <input id="txt-lookup" type="text" class="form-control" placeholder="Search for caller number..."> <span class="input-group-btn"> <button id="btn-lookup" class="btn btn-secondary" type="button">Search!</button> </span> </div> <div class="clearfix"></div>'
 	content.append(input_html);
+
+	//Wireup events to search textbox and button
 	var btn = content.find('#btn-lookup');
 	var txt = content.find('#txt-lookup');
-/*	console.log(btn);*/
 	btn.click(function() {
 		frappe.call({
 			method: "refreshednow_erpnext.api.get_caller_number",
@@ -25,6 +24,7 @@ frappe.pages['customer-lookup'].on_page_load = function(wrapper) {
 			freeze: true,
 			freeze_message: __("Retrieving Caller Details"),
 			callback: function(r) {
+				page.wrapper.find("#rn-ccc").remove();
 				content.append(frappe.render_template("caller_information", {"caller_info": r.message}));
 
 				lookup_call_lead(content);
@@ -63,34 +63,19 @@ function lookup_call_customer(content){
 	var txt = content.find('#txt-lookup');
 	var create_btn = content.find('#btn-create-customer');
 	create_btn.click(function(r){
-		/*var d = new frappe.ui.Dialog({
-		'fields': [
-		{'fieldname': 'customer', 'fieldtype': 'Link', 'options':'Customer','label':'Customer'}
-		],
-		primary_action: function(){
-		d.hide();
-		show_alert(d.get_values());
-		},
-		'primary_label': "Select"
-		});
-		d.show();*/
 		frappe.prompt([
 			{'fieldname': 'customer', 'fieldtype': 'Link', 'options':'Customer','label':'Customer'}
 		],
 		function(values){
-			/*console.log(values)*/
 			create_customer(values.customer, $(txt).val() )
 		},
 		'Select Customer',
 		'Select'
 		)
-
 	});
 }
 
-
-
-function create_customer(customer_name , caller_number){
+function create_customer(customer_name, caller_number){
 	frappe.call({
 			method:"refreshednow_erpnext.api.create_contact",
 			args:{
