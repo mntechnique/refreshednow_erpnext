9a7af7f4eed6013c0b7520791462e2a205d2a7da
Gaurav Naik <gvrvnk@gmail.com>
2016-12-30 16:01:21 +0530

    Multiple Changes
    Updated App Icon
    Updated Scheduled Service doctype
    Updated in_list_view for RN Team Member

--- refreshednow_erpnext/config/desktop.py
+++ refreshednow_erpnext/config/desktop.py
@@ -7,7 +7,7 @@ def get_data():
 		{
 			"module_name": "RefreshedNow ERPNext",
 			"color": "#53baed",
-			"icon": "icon-refresh",
+			"icon": "fa fa-refresh",
 			"type": "module",
 			"label": _("RefreshedNow ERPNext")
 		}
--- refreshednow_erpnext/refreshednow_erpnext/doctype/rn_scheduled_service/rn_scheduled_service.js
+++ refreshednow_erpnext/refreshednow_erpnext/doctype/rn_scheduled_service/rn_scheduled_service.js
@@ -3,6 +3,74 @@
 
 frappe.ui.form.on('RN Scheduled Service', {
 	refresh: function(frm) {
+		//Show service items only.
+		frappe.db.get_value("RN Settings", "RN Settings", "rn_service_item_group", function(r) {
+			cur_frm.set_query("service_type", function() {
+				return {
+					"filters": {
+						"item_group": r.rn_service_item_group,
+					}
+				};
+			});	
+		})
+		//Filter dropdowns by Customer and pertinent criteria.
+		cur_frm.set_query("billing_address", function() {
+			return {
+				"filters": {
+					"customer": frm.doc.customer,
+					"address_type": "Billing"
+				}
+			};
+		});
+		cur_frm.set_query("service_address", function() {
+			return {
+				"filters": {
+					"customer": frm.doc.customer,
+					"address_type": "Shipping"
+				}
+			};
+		});
+		cur_frm.set_query("vehicle", function() {
+			return {
+				"filters": {
+					"rn_customer": frm.doc.customer
+				}
+			};
+		});
 
+		render_vehicles(frm);
+	},
+	customer: function(frm) {
+		fetch_and_set_addresses(frm);		
+	},
+	vehicle: function(frm) {
+		render_vehicles(frm);		
 	}
 });
+
+function render_vehicles(frm) {
+	$(frm.fields_dict['vehicle_details_html'].wrapper)
+				.html("<div class='text-muted text-center' style='padding-top:5%;'>Please select a vehicle.</div>");
+
+	if (frm.doc.vehicle) {
+		frappe.model.with_doc("Vehicle", frm.doc.vehicle, function(vehicle_name) {
+			var vehicle = frappe.model.get_doc("Vehicle", vehicle_name);
+			$(frm.fields_dict['vehicle_details_html'].wrapper)
+				.html(frappe.render_template("customer_vehicle", {"customer_vehicle": [vehicle]}));
+		});
+	}
+}
+
+function fetch_and_set_addresses(frm) {
+	frappe.db.get_value(
+		"Address",
+		{ "customer":frm.doc.customer, "address_type":"Billing", "is_primary_address":true }, 
+		"name", 
+		function(r) {
+			if (r) {
+				//console.log(r);
+				frm.set_value("billing_address", r.name);	
+			}
+		}
+	);
+}
