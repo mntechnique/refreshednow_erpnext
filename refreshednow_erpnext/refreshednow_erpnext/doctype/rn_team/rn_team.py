bbb972b05eeb2bdb1f64c58e9be5d3bc39013a6f
Gaurav Naik <gvrvnk@gmail.com>
2016-12-31 17:44:55 +0530

    Added validations on RN Team doctype

--- refreshednow_erpnext/refreshednow_erpnext/doctype/rn_team/rn_team.py
+++ refreshednow_erpnext/refreshednow_erpnext/doctype/rn_team/rn_team.py
@@ -7,4 +7,18 @@ import frappe
 from frappe.model.document import Document
 
 class RNTeam(Document):
-	pass
+	def validate(self):
+		self.validate_teams()
+
+	def validate_teams(self):
+		member_emps = []
+
+		for member in self.members:
+			member_emps.append(member.member)
+
+			containing_teams = frappe.get_all("RN Team Member", filters=[["member", "=", member.member],["parent", "!=", self.name]], fields=['parent'])
+			if len(containing_teams) > 0:
+				frappe.throw("Member {0} already exists in team {1}".format(member.member_name, containing_teams[0].parent))
+		
+		if len(member_emps) != len(set(member_emps)):
+			frappe.throw("There are duplicate members in this team!")	 
