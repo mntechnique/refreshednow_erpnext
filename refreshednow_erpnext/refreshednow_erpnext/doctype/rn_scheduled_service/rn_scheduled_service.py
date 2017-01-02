c252a382595c8832b9834dd248e877f264408075
Gaurav Naik <gvrvnk@gmail.com>
2017-01-02 16:21:36 +0530

    Added overlap validation for RNSS

--- refreshednow_erpnext/refreshednow_erpnext/doctype/rn_scheduled_service/rn_scheduled_service.py
+++ refreshednow_erpnext/refreshednow_erpnext/doctype/rn_scheduled_service/rn_scheduled_service.py
@@ -9,6 +9,9 @@ from frappe.utils import add_days, nowdate
 from frappe import _
 
 class RNScheduledService(Document):
+	def validate(self):
+		self.check_overlap()
+
 	def create_so(self):
 		defaults_temp = frappe.defaults.get_defaults()
 
@@ -37,3 +40,35 @@ class RNScheduledService(Document):
 			frappe.throw(_("Sales Order was not saved. <br/> %s" % (e)))
 		else:
 			return so.name
+
+	def check_overlap(self):
+		# existing = frappe.db.sql("""select name from `tabRN Scheduled Service` as rnss where
+		# 		(
+		# 			('{starts_on}' > rnss.starts_on and '{starts_on}' < rnss.ends_on) or
+		# 			('{ends_on}' > rnss.starts_on and '{ends_on}' < rnss.ends_on) or
+		# 			('{starts_on}' <= rnss.starts_on and '{ends_on}' >= rnss.ends_on)
+		# 		)
+		# 	and rnss.name!='{name}'""".format(
+		# 		starts_on=self.starts_on,
+		# 		ends_on=self.ends_on,
+		# 		name=self.name
+		# 	), as_dict=True)
+
+		# if len(existing) > 0:
+		# 	frappe.throw("This service will overlap with {0} other service(s).".format(len(existing)))sta
+		print "-----"
+		print self.team
+		print "-----"
+		existing_services = frappe.get_all("RN Scheduled Service", filters={"team": self.team}, fields=["name", "starts_on", "ends_on"])
+		print existing_services
+		print "-----"
+
+		for ss in existing_services:
+			starts_on = frappe.utils.data.get_datetime(self.starts_on)
+			ends_on = frappe.utils.data.get_datetime(self.ends_on)
+
+			if (starts_on > ss.starts_on and starts_on < ss.ends_on) or \
+				(ends_on > ss.starts_on and ends_on < ss.ends_on) or \
+				(starts_on <= ss.starts_on and ends_on >= ss.ends_on):
+				frappe.throw("This service overlaps with {0}".format(ss.name))
+
