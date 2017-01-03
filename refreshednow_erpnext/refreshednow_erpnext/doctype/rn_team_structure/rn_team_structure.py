4bd2b2626a7aae026f5157dcd6c03ab8b4c4df6b
Revant Nandgaonkar <revant.one@gmail.com>
2017-01-03 12:20:36 +0530

    Validation on Team Structure

--- refreshednow_erpnext/refreshednow_erpnext/doctype/rn_team_structure/rn_team_structure.py
+++ refreshednow_erpnext/refreshednow_erpnext/doctype/rn_team_structure/rn_team_structure.py
@@ -7,4 +7,15 @@ import frappe
 from frappe.model.document import Document
 
 class RNTeamStructure(Document):
-	pass
+	def validate(self):
+		self.validate_designations_and_strength()
+
+	def validate_designations_and_strength((self):
+		designation_list = []
+		for structure in self.team_structure:
+			designation_list.append(structure.designation)
+			if structure.strength <= 0:
+				frappe.throw(_("Strength must be positive number"))
+
+		if len(designation_list)!= len(set(designation_list)):
+			frappe.throw(_("Same designation is entered more than once"))
